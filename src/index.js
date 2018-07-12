d8921ae0918dba257c75781bdb3dcbb59a776560
KamilaKisiel <kisiel.kamila@gmail.com>
2018-07-12 20:06:56 +0200

    tachyon installed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
+import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
