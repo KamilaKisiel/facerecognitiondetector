dcebe1e4038af51552a13ada468d6bb733c228a0
KamilaKisiel <kisiel.kamila@gmail.com>
2018-07-15 21:09:56 +0200

    adding parameters to particles bckground

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    }
+}
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particles'
+              params={particlesOptions}
+            />
         <Navigation />
         <Logo />
         <Rank />
