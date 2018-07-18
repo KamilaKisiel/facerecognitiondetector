b106266c5184f319b0e5af7533e825a48fc126d4
KamilaKisiel <kisiel.kamila@gmail.com>
2018-07-18 21:19:29 +0200

    coordinates for face box

--- src/App.js
+++ src/App.js
@@ -48,7 +48,12 @@ class App extends Component {
     const image = document.getElementById('inputImage')
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
   }
 
   onInputChange = (event) => {
